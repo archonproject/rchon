a39b3e4eb772a56393702472542d69e8458dde74
dopemaster <depp.joe06@gmail.com>
2012-09-28 12:51:09 -0500

    Update app/models/collection_creator_relationship.rb
    added relationships for collection and creator

--- app/models/collection_creator_relationship.rb
+++ app/models/collection_creator_relationship.rb
@@ -1,3 +1,7 @@
 class CollectionCreatorRelationship < ActiveRecord::Base
   attr_accessible :collection_id, :creator_id, :primary_creator
+  has_one :collection
+  has_one :creator
+  validates_presence_of :collection
+  validates_presence_of :creator
 end
