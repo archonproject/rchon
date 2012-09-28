c587b95a880cad2beb02a4770d04edd0a42c082f
dopemaster <depp.joe06@gmail.com>
2012-09-28 11:33:10 -0500

    Update app/models/book.rb
    each book belongs to a collection.Null values allowed.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
   attr_accessible :copy_edition, :description, :edition, :notes, :number_of_pages, :place_of_publication, :publication_date, :publisher, :series, :title
+  belongs_to :collection
 end
