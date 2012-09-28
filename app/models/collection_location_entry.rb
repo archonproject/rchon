ddb66f23d481c022a96dcb60f176df421d018e11
dopemaster <depp.joe06@gmail.com>
2012-09-28 12:52:21 -0500

    Update app/models/collection_location_entry.rb
    changed validates_associated to validates_presence_of for collection and location

--- app/models/collection_location_entry.rb
+++ app/models/collection_location_entry.rb
@@ -2,6 +2,6 @@ class CollectionLocationEntry < ActiveRecord::Base
   attr_accessible :collection_id, :content, :extent, :extent_unit_id, :location_id, :range_value, :section, :shelf
   has_one :location
   has_one :collection
-  validates_associated :location
-  validates_associated :collection
+  validates_presence_of :location
+  validates_presence_of :collection
 end
