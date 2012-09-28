a0094c102177e72e5f89cb7986e77f2234461acb
dopemaster <depp.joe06@gmail.com>
2012-09-28 13:13:27 -0500

    Update app/models/creator_book_relationship.rb
    added has one relationships

--- app/models/creator_book_relationship.rb
+++ app/models/creator_book_relationship.rb
@@ -1,5 +1,7 @@
 class CreatorBookRelationship < ActiveRecord::Base
   attr_accessible :book_id, :creator_id, :primary_creator
+  has_one :book
+  has_one :creator
   validates_presence_of :book_id
   validates_presence_of :creator_id
 end
