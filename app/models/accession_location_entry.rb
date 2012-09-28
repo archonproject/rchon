255048753e2084438bbc9bba1ba905192524950c
dopemaster <depp.joe06@gmail.com>
2012-09-28 12:48:14 -0500

    Update app/models/accession_location_entry.rb
    changed to validates presence of, since location and accession cannot be null

--- app/models/accession_location_entry.rb
+++ app/models/accession_location_entry.rb
@@ -2,6 +2,6 @@ class AccessionLocationEntry < ActiveRecord::Base
   attr_accessible :accession_id, :content, :extent, :extent_unit_id, :location_id, :range_value, :section, :shelf
   has_one :location
   has_one :accession
-  validates_associated :location
-  validates_associated :accession
+  validates_presence_of :location
+  validates_presence_of :accession
   end
