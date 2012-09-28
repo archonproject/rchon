6ae8ea5f7c33915072caf4994cdca6ad6136ac90
dopemaster <depp.joe06@gmail.com>
2012-09-28 11:28:58 -0500

    Update app/models/accession_location_entry.rb
    Each Accession location index one location id and accession id associated with it.

--- app/models/accession_location_entry.rb
+++ app/models/accession_location_entry.rb
@@ -1,3 +1,7 @@
 class AccessionLocationEntry < ActiveRecord::Base
   attr_accessible :accession_id, :content, :extent, :extent_unit_id, :location_id, :range_value, :section, :shelf
-end
+  has_one :location
+  has_one :accession
+  validates_associated :location
+  validates_associated :accession
+  end
