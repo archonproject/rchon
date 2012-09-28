6c31059d4cb77a354ef36f5cefee8bc9d862f1c1
dopemaster <depp.joe06@gmail.com>
2012-09-28 13:12:23 -0500

    Update app/models/creator_relationship.rb
    added validates presence of relationships

--- app/models/creator_relationship.rb
+++ app/models/creator_relationship.rb
@@ -1,3 +1,5 @@
 class CreatorRelationship < ActiveRecord::Base
   attr_accessible :creator_id, :creator_relationship_type_id, :description, :related_creator_id
+  validates_presence_of :creator_id
+  validates_presence_of :related_creator_id
 end
