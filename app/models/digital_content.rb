777b50acdcd5f3bd2a09f17541d80362d67035c5
dopemaster <depp.joe06@gmail.com>
2012-09-28 13:33:33 -0500

    Update app/models/digital_content.rb
    added belongs to relationships

--- app/models/digital_content.rb
+++ app/models/digital_content.rb
@@ -1,3 +1,5 @@
 class DigitalContent < ActiveRecord::Base
   attr_accessible :browsable, :collection_content_id, :collection_id, :content_url, :contributor, :date, :hyperlink_url, :identifier, :physical_description, :publisher, :rights_statement, :scope, :title
+  belongs_to :collection
+  belongs_to :collection_content
 end
