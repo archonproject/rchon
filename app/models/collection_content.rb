0ce467c47a9241deeca53cad8a4d58785080dcb8
dopemaster <depp.joe06@gmail.com>
2012-09-28 11:51:00 -0500

    Update app/models/collection_content.rb
    Collection content belongs to collection

--- app/models/collection_content.rb
+++ app/models/collection_content.rb
@@ -1,3 +1,4 @@
 class CollectionContent < ActiveRecord::Base
   attr_accessible :collection_id, :contains_content, :date, :description, :enabled, :level_container_id, :level_container_identifier, :parent_id, :private_title, :root_content_id, :sort_order, :title
+  belongs_to :collection
 end
