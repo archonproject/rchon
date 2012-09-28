04cf56e2047be2a92c6de54ed661f55ab7b57bec
dopemaster <depp.joe06@gmail.com>
2012-09-28 13:10:42 -0500

    Update app/models/creator_book_relationship.rb
    added validates_presence_of relationships for book id and creator id fields

--- app/models/creator_book_relationship.rb
+++ app/models/creator_book_relationship.rb
@@ -1,3 +1,5 @@
 class CreatorBookRelationship < ActiveRecord::Base
   attr_accessible :book_id, :creator_id, :primary_creator
+  validates_presence_of :book_id
+  validates_presence_of :creator_id
 end
