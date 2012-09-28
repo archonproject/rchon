fbefb67483ee180b179adf629a01aaf48d9231b8
dopemaster <depp.joe06@gmail.com>
2012-09-28 13:16:45 -0500

    Update app/models/creator_accession_relationship.rb
    added relations for accession_id

--- app/models/creator_accession_relationship.rb
+++ app/models/creator_accession_relationship.rb
@@ -1,3 +1,5 @@
 class CreatorAccessionRelationship < ActiveRecord::Base
   attr_accessible :accession_id, :classification_id, :collection_id, :primary_collection
+  has_one :accession
+  validates_presence_of :accession_id
 end
