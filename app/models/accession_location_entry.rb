6d39f88b22133d581f38f8c80b1772ac46733d13
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:03:51 -0500

    Update app/models/accession_location_entry.rb
    added belongs to relationships

--- app/models/accession_location_entry.rb
+++ app/models/accession_location_entry.rb
@@ -1,7 +1,5 @@
 class AccessionLocationEntry < ActiveRecord::Base
   attr_accessible :accession_id, :content, :extent, :extent_unit_id, :location_id, :range_value, :section, :shelf
-  has_one :location
-  has_one :accession
-  validates_presence_of :location
-  validates_presence_of :accession
+  belongs_to :location
+  belongs_to :accession
   end
