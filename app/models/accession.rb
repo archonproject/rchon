a77925dd5e8b405a38fe726c4f9e59844f8936ad
dopemaster <depp.joe06@gmail.com>
2012-10-01 13:55:33 -0500

    Update app/models/accession.rb
    added belongs to relationship for material type

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -1,5 +1,5 @@
 class Accession < ActiveRecord::Base
   attr_accessible :accession_date, :comments, :donor, :donor_contact_info, :donor_notes, :enabled, :expected_completion_date, :identifier, :inclusive_dates, :material_type_id, :physical_description, :processing_priority_id, :received_extent, :received_extent_unit_id, :scope_content, :title, :unprocessed_extent, :unprocessed_extent_unit_id
-  has_one :material_type
+  belongs_to :material_type
 
 end
