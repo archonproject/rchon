02173afc4ad5cfb8425d4e7af2bda8144ebdaeba
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:53:54 -0500

    Update app/models/research_appointment.rb
    added belongs to researcher relationship

--- app/models/research_appointment.rb
+++ app/models/research_appointment.rb
@@ -1,4 +1,5 @@
 class ResearchAppointment < ActiveRecord::Base
   attr_accessible :appointment_purpose_id, :archivist_comments, :arrival_time, :departure_time, :researcher_comments, :researcher_id, :submit_time, :topic
   belongs_to :research_appointment_purpose
+  belongs_to :researcher
 end
