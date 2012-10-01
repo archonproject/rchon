1654f8207159fa0cc69e5f674ea64a94804eb6fd
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:33:03 -0500

    Update app/models/creator.rb
    added has many creator relationships

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,5 +2,6 @@ class Creator < ActiveRecord::Base
   attr_accessible :biog_hist, :biog_hist_author, :creator_source_id, :creator_type_id, :dates, :identifier, :language_id, :lcnaf_compliant, :lcnaf_dates, :name, :name_fuller_Form, :name_variants, :respository_id, :script_id, :sources
   has_many :classifications
   has_many :collection_creator_relationships
-  :has_many :creatorBookRelationships
+  has_many :creatorBookRelationships
+  has_many :creatorRelationships
 end
