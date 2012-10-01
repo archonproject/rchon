d0cca20990577bf3e1d6a9244de16e27fecc5054
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:07:50 -0500

    Update app/models/accession.rb
    added has many relationship for accession location entries

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -1,5 +1,6 @@
 class Accession < ActiveRecord::Base
   attr_accessible :accession_date, :comments, :donor, :donor_contact_info, :donor_notes, :enabled, :expected_completion_date, :identifier, :inclusive_dates, :material_type_id, :physical_description, :processing_priority_id, :received_extent, :received_extent_unit_id, :scope_content, :title, :unprocessed_extent, :unprocessed_extent_unit_id
   belongs_to :material_type
+  has_many :accessionLocationEntries
 
 end
