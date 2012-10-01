6b17049efd09df72f64801caf8df4533308f16c2
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:31:50 -0500

    Update app/models/creator_book_relationship.rb
    added belongs to relationships

--- app/models/creator_book_relationship.rb
+++ app/models/creator_book_relationship.rb
@@ -1,7 +1,7 @@
 class CreatorBookRelationship < ActiveRecord::Base
   attr_accessible :book_id, :creator_id, :primary_creator
-  has_one :book
-  has_one :creator
+  belongs_to :book
+  belongs_to :creator
   validates_presence_of :book_id
   validates_presence_of :creator_id
 end
