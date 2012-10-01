872d845b0889eb7c260aceb8c49a68175f6d0e64
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:36:15 -0500

    Update app/models/collection_content.rb
    added has_many digitalContents

--- app/models/collection_content.rb
+++ app/models/collection_content.rb
@@ -2,4 +2,5 @@ class CollectionContent < ActiveRecord::Base
   attr_accessible :collection_id, :contains_content, :date, :description, :enabled, :level_container_id, :level_container_identifier, :parent_id, :private_title, :root_content_id, :sort_order, :title
   belongs_to :collection
   belongs_to :level_container
+  has_many :digitalContents
 end
