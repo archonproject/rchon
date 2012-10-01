ecb9ed57b30bf62d816f2e3e3bcc0d567f54b9c6
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:26:53 -0500

    Update app/models/accession.rb
    added has_many creator accession relationships

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -2,5 +2,6 @@ class Accession < ActiveRecord::Base
   attr_accessible :accession_date, :comments, :donor, :donor_contact_info, :donor_notes, :enabled, :expected_completion_date, :identifier, :inclusive_dates, :material_type_id, :physical_description, :processing_priority_id, :received_extent, :received_extent_unit_id, :scope_content, :title, :unprocessed_extent, :unprocessed_extent_unit_id
   belongs_to :material_type
   has_many :accessionLocationEntries
+  has_many :creator_accession_relationships
 
 end
