371bbdb15518d06e87b093743820960e5723a223
dopemaster <depp.joe06@gmail.com>
2012-10-01 14:48:25 -0500

    Update app/models/resarch_cart.rb
    deleted belongs to researcher relationship, since researcher model doesnt exist

--- app/models/resarch_cart.rb
+++ app/models/resarch_cart.rb
@@ -2,5 +2,4 @@ class ResarchCart < ActiveRecord::Base
   attr_accessible :collection_content_id, :collection_id, :researcher_id
   belongs_to :collection
   belongs_to :collection_content
-  belongs_to :researcher
 end
