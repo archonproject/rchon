93a126042da721d770308a030094ccf6653e7582
sugandhaa <sugandha409@gmail.com>
2012-10-18 11:49:21 -0500

    Update app/models/location.rb
    Added through accessions

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,5 @@ class Location < ActiveRecord::Base
   attr_accessible :description, :name, :repository_limit
   has_many :accessionLocationEntries
   has_many :collectionLocationEntries
+  has_many :accessions, :through => :accessionLocationEntries
 end
