05973e1fc57fe0ecedf9d47aa9ea728f30298df5
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:00:53 -0500

    Update app/models/collection_creator_relationship.rb
    changed to belong_to

--- app/models/collection_creator_relationship.rb
+++ app/models/collection_creator_relationship.rb
@@ -1,7 +1,7 @@
 class CollectionCreatorRelationship < ActiveRecord::Base
   attr_accessible :collection_id, :creator_id, :primary_creator
-  has_one :collection
-  has_one :creator
+  belongs_to :collection
+  belongs_to :creator
   validates_presence_of :collection_id
   validates_presence_of :creator_id
 end
