4e4cb5fa5b202d2000316f7f96342baf470ac119
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:32:44 -0500

    Update app/models/book.rb
    dependant added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
   attr_accessible :copy_edition, :description, :edition, :notes, :number_of_pages, :place_of_publication, :publication_date, :publisher, :series, :title
   belongs_to :collection
-  has_many :creatorBookRelationships
+  has_many :creatorBookRelationships ,:dependent => :destroy
   has_many :creators , :through =>  :creatorBookRelationships
 end
