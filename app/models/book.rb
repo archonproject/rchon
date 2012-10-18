1aac6a60fc03c296bb00b3db5bba1460b9fbb074
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:11:40 -0500

    Update app/models/book.rb
    added through creator book relation ships

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   attr_accessible :copy_edition, :description, :edition, :notes, :number_of_pages, :place_of_publication, :publication_date, :publisher, :series, :title
   belongs_to :collection
   has_many :creatorBookRelationships
+  has_many :creators , :through =>  :creatorBookRelationships
 end
