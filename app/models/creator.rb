d719d5bb147125f2f4186dec48a7b657ecb86105
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:36:09 -0500

    Update app/models/creator.rb
    dependants

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,10 +1,10 @@
 class Creator < ActiveRecord::Base
   attr_accessible :biog_hist, :biog_hist_author, :creator_source_id, :creator_type_id, :dates, :identifier, :language_id, :lcnaf_compliant, :lcnaf_dates, :name, :name_fuller_Form, :name_variants, :respository_id, :script_id, :sources
   has_many :classifications
-  has_many :collection_creator_relationships
-  has_many :creatorBookRelationships
-  has_many :creatorRelationships
-  has_many :digitalContentCreatorRelationships
+  has_many :collection_creator_relationships ,:dependent => :destroy
+  has_many :creatorBookRelationships ,:dependent => :destroy
+  has_many :creatorRelationships 
+  has_many :digitalContentCreatorRelationships ,:dependent => :destroy
   has_many :collections , :through => :collection_creator_relationships 
   has_many :books , :through => :creatorBookRelationships
   has_many :digitalContents , :through => :digitalContentCreatorRelationships
