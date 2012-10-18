972e2e50c801e18d6be690bd03bc7a127f8f88a9
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:36:44 -0500

    Update app/models/digital_content.rb
    added dependant

--- app/models/digital_content.rb
+++ app/models/digital_content.rb
@@ -2,7 +2,7 @@ class DigitalContent < ActiveRecord::Base
   attr_accessible :browsable, :collection_content_id, :collection_id, :content_url, :contributor, :date, :hyperlink_url, :identifier, :physical_description, :publisher, :rights_statement, :scope, :title
   belongs_to :collection
   belongs_to :collection_content
-  has_many :digitalContentCreatorRelationships
+  has_many :digitalContentCreatorRelationships ,:dependent => :destroy
   has_one :digital_content_file
   has_many :creators , :through => :digitalContentCreatorRelationships
 end
