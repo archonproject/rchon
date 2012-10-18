ac3935ddf997fe1cb455aaa1618fb128be2fba9a
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:46:18 -0500

    Update app/models/location.rb
    through and dependants added

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :description, :name, :repository_limit
-  has_many :accessionLocationEntries
-  has_many :collectionLocationEntries
+  has_many :accessionLocationEntries ,:dependent => :destroy
+  has_many :collectionLocationEntries ,:dependent => :destroy
   has_many :accessions, :through => :accessionLocationEntries
+  has_many :collections, :through=> :collectionLocationEntries
 end
