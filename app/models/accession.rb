87ef58b60bc98b1877da68b239480eb42e9abbbf
sugandhaa <sugandha409@gmail.com>
2012-10-18 12:31:31 -0500

    Update app/models/accession.rb
    depedants added

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -1,8 +1,8 @@
 class Accession < ActiveRecord::Base
   attr_accessible :accession_date, :comments, :donor, :donor_contact_info, :donor_notes, :enabled, :expected_completion_date, :identifier, :inclusive_dates, :material_type_id, :physical_description, :processing_priority_id, :received_extent, :received_extent_unit_id, :scope_content, :title, :unprocessed_extent, :unprocessed_extent_unit_id
   belongs_to :material_type
-  has_many :accessionLocationEntries
-  has_many :creator_accession_relationships
+  has_many :accessionLocationEntries ,:dependent => :destroy
+  has_many :creator_accession_relationships,:dependent => :destroy
   has_many :locations, :through => :accessionLocationEntries
 
 end
