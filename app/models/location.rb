9b71e35dee77c501adf4901f4c4e20ec92b311e4
sugandhaa <sugandha409@gmail.com>
2012-10-19 14:22:32 -0500

    Update app/models/location.rb
    added under_score case 

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :description, :name, :repository_limit
-  has_many :accessionLocationEntries ,:dependent => :destroy
-  has_many :collectionLocationEntries ,:dependent => :destroy
-  has_many :accessions, :through => :accessionLocationEntries
-  has_many :collections, :through=> :collectionLocationEntries
+  has_many :accession_location_entries ,:dependent => :destroy
+  has_many :collection_location_entries ,:dependent => :destroy
+  has_many :accessions, :through => :accession_location_entries
+  has_many :collections, :through=> :collection_location_entries
 end
