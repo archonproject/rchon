25f66f9ddaf32824f0ce9b29b01b76ac52eb0e2a
sugandhaa <sugandha409@gmail.com>
2012-10-19 14:21:27 -0500

    Update app/models/digital_content.rb
    under_score case added

--- app/models/digital_content.rb
+++ app/models/digital_content.rb
@@ -2,7 +2,7 @@ class DigitalContent < ActiveRecord::Base
   attr_accessible :browsable, :collection_content_id, :collection_id, :content_url, :contributor, :date, :hyperlink_url, :identifier, :physical_description, :publisher, :rights_statement, :scope, :title
   belongs_to :collection
   belongs_to :collection_content
-  has_many :digitalContentCreatorRelationships ,:dependent => :destroy
+  has_many :digital_content_creator_relationships ,:dependent => :destroy
   has_one :digital_content_file
-  has_many :creators , :through => :digitalContentCreatorRelationships
+  has_many :creators , :through => :digital_content_creator_relationships
 end
