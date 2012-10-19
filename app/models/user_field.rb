8a6ad52de702cb71eaac09a3198d6a2803258119
sugandhaa <sugandha409@gmail.com>
2012-10-19 14:28:05 -0500

    Update app/models/user_field.rb
    camel case

--- app/models/user_field.rb
+++ app/models/user_field.rb
@@ -1,4 +1,4 @@
 class UserField < ActiveRecord::Base
   attr_accessible :collection_content_id, :ead_element_id, :title, :value
-  belongs_to :collection_content
+  belongs_to :collectionContent
 end
