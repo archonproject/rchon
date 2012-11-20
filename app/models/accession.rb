05ac80b93e0a8360cb2ac9fd79c1837426e27954
sugandhaa <sugandha409@gmail.com>
2012-11-20 15:48:06 -0600

    Update app/models/accession.rb
    added belongs to processing  priority and extent unit
    and has_many subjects creators and collections

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -1,8 +1,13 @@
 class Accession < ActiveRecord::Base
   attr_accessible :accession_date, :comments, :donor, :donor_contact_info, :donor_notes, :enabled, :expected_completion_date, :identifier, :inclusive_dates, :material_type_id, :physical_description, :processing_priority_id, :received_extent, :received_extent_unit_id, :scope_content, :title, :unprocessed_extent, :unprocessed_extent_unit_id
   belongs_to :material_type
+  belongs_to :processing_priority
+  belongs_to :extent_unit
   has_many :accession_location_entries ,:dependent => :destroy
   has_many :creator_accession_relationships,:dependent => :destroy
   has_many :locations, :through => :accession_location_entries
+  has_many :subjects, :through => :accession_subject_relationships
+  has_many :creators, :through => :creator_accession_relationships
+  has_many :collections, :through => :accession_collection_relationships
 
 end
