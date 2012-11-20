3e9c8ba4c12948f1e14e9ff73428102c2be5aac6
sugandhaa <sugandha409@gmail.com>
2012-11-20 16:12:14 -0600

    Update app/models/subject.rb
    addded has many accessions

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,5 @@
 class Subject < ActiveRecord::Base
   attr_accessible :description, :identifier, :parent_id, :subject, :subject_source_id, :subject_type_id
-  belongs_to :subjectSource
+  belongs_to :subject_source
+  has_many :accessions, :through => :accession_subject_relationships
 end
