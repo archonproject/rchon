693cece40d40890faf81e9ae8f4a01c709b883ff
sugandhaa <sugandha409@gmail.com>
2012-11-20 16:14:37 -0600

    Update app/models/creator.rb
    has many accessions , creator_accession_relationships

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -3,9 +3,11 @@ class Creator < ActiveRecord::Base
   has_many :classifications
   has_many :collection_creator_relationships ,:dependent => :destroy
   has_many :creator_book_relationships ,:dependent => :destroy
-  has_many :creatorRelationships 
+  has_many :creator_relationships
+  has_many :creator_accession_relationships, :dependent => destroy
   has_many :digital_content_creator_relationships ,:dependent => :destroy
   has_many :collections , :through => :collection_creator_relationships 
   has_many :books , :through => :creator_book_relationships
   has_many :digitalContents , :through => :digital_content_creator_relationships
+  has_many :accessions, :through => :creator_accession_relationships
 end
