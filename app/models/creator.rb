a8cc01579acf40b38ed87994266d677343afeaae
sugandhaa <sugandha409@gmail.com>
2012-11-21 13:44:25 -0600

    Update app/models/creator.rb
    belongs to repository

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,5 +1,6 @@
 class Creator < ActiveRecord::Base
   attr_accessible :biog_hist, :biog_hist_author, :creator_source_id, :creator_type_id, :dates, :identifier, :language_id, :lcnaf_compliant, :lcnaf_dates, :name, :name_fuller_Form, :name_variants, :respository_id, :script_id, :sources
+  belongs_to :repository
   has_many :classifications
   has_many :collection_creator_relationships ,:dependent => :destroy
   has_many :creator_book_relationships ,:dependent => :destroy
