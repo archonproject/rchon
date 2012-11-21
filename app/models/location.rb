b24ae01fe6584df9c37e90a0ac66719b3929953a
sugandhaa <sugandha409@gmail.com>
2012-11-21 13:27:21 -0600

    Update app/models/location.rb
    has many repositories, repository relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :description, :name, :repository_limit
   has_many :accession_location_entries ,:dependent => :destroy
   has_many :collection_location_entries ,:dependent => :destroy
+  has_many :location_repository_relationships, :dependent => :destroy
   has_many :accessions, :through => :accession_location_entries
-  has_many :collections, :through=> :collection_location_entries
+  has_many :collections, :through => :collection_location_entries
+  has_many :repositories, :through => :location_repository_relationships
 end
