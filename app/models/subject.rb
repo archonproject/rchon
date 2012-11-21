5acc70e13679c6787578b8bf01b879083908d524
sugandhaa <sugandha409@gmail.com>
2012-11-21 15:24:01 -0600

    Update app/models/subject.rb
    has many collection content relationships, belongs to subject_type

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,10 +1,13 @@
 class Subject < ActiveRecord::Base
   attr_accessible :description, :identifier, :parent_id, :subject, :subject_source_id, :subject_type_id
   belongs_to :subject_source
+  belongs_to :subject_type
   has_many :accession_subject_relationships, :dependent => :destroy
   has_many :collection_subject_relationships, :dependent => :destroy
+  has_many :collection_content_subject_relationships, :dependent => :destroy
   has_many :digital_content_subject_relationships, :dependent => :destroy
   has_many :accessions, :through => :accession_subject_relationships
   has_many :collections, :through => :collection_subject_relationships 
   has_many :digital_contents, :through => :digital_content_subject_relationships
+  has_many :collection_contents, :through => :collection_content_subject_relationships
 end
