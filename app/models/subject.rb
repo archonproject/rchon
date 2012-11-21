692ed1d5ede5bc4ab4df89446461ed5f7a0cb67c
sugandhaa <sugandha409@gmail.com>
2012-11-21 12:52:40 -0600

    Update app/models/subject.rb
    has many coll_sub_relationships, subjects through..

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,5 +2,7 @@ class Subject < ActiveRecord::Base
   attr_accessible :description, :identifier, :parent_id, :subject, :subject_source_id, :subject_type_id
   belongs_to :subject_source
   has_many :accession_subject_relationships, :dependent => :destroy
+  has_many :collection_subject_relationships, :dependent => :destroy
   has_many :accessions, :through => :accession_subject_relationships
+  has_many :subjects, :through => :collection_subject_relationships 
 end
