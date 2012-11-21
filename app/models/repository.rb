808a784931231d142181b53ab33af9183e665023
sugandhaa <sugandha409@gmail.com>
2012-11-21 13:43:35 -0600

    Update app/models/repository.rb
    has many collections, creators, user_repository_relationships, users

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,9 @@
 class Repository < ActiveRecord::Base
   attr_accessible :address, :address2, :administrator, :city, :code, :country, :email, :email_signature, :fax, :name, :phone, :phone_ext, :research_functionality, :state, :url, :zip
+  has_many :collections
+  has_many :creators
   has_many :location_repository_relationships, :dependent => :destroy
+  has_many :user_repository_relationships, :dependent => :destroy
+  has_many :users, :through => :user_repository_relationships
   has_many :locations, :through => :location_repository_relationships
 end
