1f8ea41b2d40dbeae095d98e1e764fd392011366
sugandhaa <sugandha409@gmail.com>
2012-11-21 14:26:50 -0600

    Update app/models/digital_content.rb
    has language and subjects relationships, 
    has files

--- app/models/digital_content.rb
+++ app/models/digital_content.rb
@@ -3,6 +3,12 @@ class DigitalContent < ActiveRecord::Base
   belongs_to :collection
   belongs_to :collection_content
   has_many :digital_content_creator_relationships ,:dependent => :destroy
+  has_many :digital_content_language_relationships, :dependent => :destroy
+  has_many :digital_content_subject_relationships, :dependent => :destroy
   has_one :digital_content_file
   has_many :creators , :through => :digital_content_creator_relationships
+  has_many :languages, :through => :digital_content_language_relationships
+  :has_many :subjects, :through => :digital_content_subject_relationships
+  has_many :files
+
 end
