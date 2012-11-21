97c51fc349434feb6da42d44f782db4cab4a4d25
sugandhaa <sugandha409@gmail.com>
2012-11-21 13:28:48 -0600

    Update app/models/repository.rb
    has many location repository relationships, and locations

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,5 @@
 class Repository < ActiveRecord::Base
   attr_accessible :address, :address2, :administrator, :city, :code, :country, :email, :email_signature, :fax, :name, :phone, :phone_ext, :research_functionality, :state, :url, :zip
+  has_many :location_repository_relationships, :dependent => :destroy
+  has_many :locations, :through => :location_repository_relationships
 end
