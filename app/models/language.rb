847cbb184081029e3b62f7dddb471ec66062e1af
sugandhaa <sugandha409@gmail.com>
2012-11-26 16:35:45 -0600

    Update app/models/language.rb
    added has many relationships

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,5 @@
 class Language < ActiveHash::Base
-
+  has_many :collections
+  has_many :collection_language_relationships, :dependent => :destroy
+  has_many :collections, :through => :collection_language_relationsips
 end
