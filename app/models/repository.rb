9b656e6bbb1e0e0d5f33c6d99184d94945923ba2
sugandhaa <sugandha409@gmail.com>
2012-11-26 16:44:45 -0600

    Update app/models/repository.rb
    added belongs to country

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,6 @@
 class Repository < ActiveRecord::Base
   attr_accessible :address, :address2, :administrator, :city, :code, :country, :email, :email_signature, :fax, :name, :phone, :phone_ext, :research_functionality, :state, :url, :zip
+  belongs_to :country
   has_many :collections
   has_many :creators
   has_many :location_repository_relationships, :dependent => :destroy
