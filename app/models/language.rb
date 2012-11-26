1c3c413b6de5f29148db7d93be19a4282e9d92fb
sugandhaa <sugandha409@gmail.com>
2012-11-26 16:40:24 -0600

    Update app/models/language.rb
    added has many creators, digital contents, digital content creator relationships

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,4 +2,7 @@ class Language < ActiveHash::Base
   has_many :collections
   has_many :collection_language_relationships, :dependent => :destroy
   has_many :collections, :through => :collection_language_relationsips
+  has_many :creators
+  has_many :digital_content_language_relationships
+  has_many :digital_contents, :through => :digital_content_creator_relationships
 end
