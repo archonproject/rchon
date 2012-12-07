e36920c9feb268f07a14a9c4e055e78c8d2e1587
sugandhaa <sugandha409@gmail.com>
2012-12-07 14:32:15 -0600

    Update app/models/user.rb
    added attr_accessible fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
  class User < ActiveRecord::Base
+  attr_accessible :login, :email, :password_hash, :first_name, :last_name, :display_name, :is_admin_user, :register_time, :pending, :pending_hash, :user_group_id, :language_id, :country_id, :require_secure_connection, :profile_digit_content_id, :repository_id, :repository_limit, :locked
   belongs_to :language
   belongs_to :country
   has_many :user_repository_relationships, :dependent => :destroy
